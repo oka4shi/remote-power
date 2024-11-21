513b4822e5a66ee74d95a6c75b9601dd25422e32
OKAMOTO Shigehiro <primary@oka4shi.dev>
2024-11-21 23:41:52 +0900

    feat: implement gpio controller

--- gpio/gpio.go
+++ gpio/gpio.go
@@ -11,6 +11,11 @@ type Port struct {
 	Pin int
 }
 
+func isPortReady(pin int) bool {
+	gpioFile, err := os.Stat(fmt.Sprintf("/sys/class/gpio/gpio%d", pin))
+	return err == nil && gpioFile.IsDir()
+}
+
 func NewPort(bank Bank, group Group, x X) (*Port, error) {
 	pin := int(bank)*32 + (int(group)*8 + int(x))
 	p := Port{
@@ -21,25 +26,39 @@ func NewPort(bank Bank, group Group, x X) (*Port, error) {
 		return nil, errors.New("Can't use GPIO")
 	}
 
-	_, err = os.Stat(fmt.Sprintf("/sys/class/gpio/gpio%d", pin))
-	if err == nil {
+	if isPortReady(pin) {
 		return &p, nil
 	}
 
 	if err := os.WriteFile("/sys/class/gpio/export", []byte(strconv.Itoa(pin)), 0644); err != nil {
-		return nil, errors.New(fmt.Sprintf("Failed to initialize gpio%d", pin))
+		return nil, errors.New("Failed to initialize GPIO")
 	}
 	return &p, nil
 }
 
-func (p *Port) SetDirection(value Direction) error {
+var directionMap = map[Direction]string{
+	IN:  "in",
+	OUT: "out",
+}
 
-	// TODO
+func (p *Port) SetDirection(value Direction) error {
+	if !isPortReady(p.Pin) {
+		return errors.New("The port isn't initialized")
+	}
+	if err := os.WriteFile(fmt.Sprintf("/sys/class/gpio/gpio%d/direction", p.Pin), []byte(directionMap[value]), 0644); err != nil {
+		return errors.New("Failed to set the direction")
+	}
+
 	return nil
 }
 
 func (p *Port) DegitalWrite(value OutLevel) error {
+	if !isPortReady(p.Pin) {
+		return errors.New("The port isn't initialized")
+	}
+	if err := os.WriteFile(fmt.Sprintf("/sys/class/gpio/gpio%d/value", p.Pin), []byte(fmt.Sprintf("%d", value)), 0644); err != nil {
+		return errors.New("Failed to set value")
+	}
 
-	// TODO
 	return nil
 }
