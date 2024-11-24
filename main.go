0f9800ce794586bdbf00bb5d4c46fd7240098c8e
OKAMOTO Shigehiro <primary@oka4shi.dev>
2024-11-24 16:52:23 +0900

    implement client

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import (
 )
 
 func main() {
-
 	http.HandleFunc("GET /", server.Home)
-	//http.HandleFunc("POST /power", server.Home)
+	http.HandleFunc("POST /push", server.Push)
+	http.HandleFunc("GET /push/status", server.PushStatus)
 	http.Handle("GET /static/", http.StripPrefix("/static/", http.FileServer(http.Dir("server/template/static"))))
 
 	log.Print(http.ListenAndServe(":8080", nil))
---
+++ server/template/static/script.js
@@ -0,0 +1,77 @@
+/** @type {HTMLButtonElement | null}*/
+const pushButton = document.getElementById("push")
+/** @type {HTMLButtonElement | null}*/
+const longPushButton = document.getElementById("long-push")
+const message = document.getElementById("message")
+
+/** @param {string} path
+*   @param {RequestInit} init 
+*/
+async function fetchAPI(path, init) {
+    let error = ""
+
+    const res = await fetch(path, init).then((resp) => {
+        if (!resp.ok) {
+            if (resp.status == 409) {
+                error = "現在実行中です！"
+                return ""
+            }
+            error = "エラーが発生しました！"
+            return ""
+        }
+        return resp.text()
+    }).catch((err) => {
+        error = `通信中にエラーが発生しました: ${err}`
+        return ""
+    })
+
+    if (error) {
+        return new Error(error)
+    }
+
+    return res
+}
+
+/** @param {boolean} isLong */
+async function buttonClickHandler(isLong) {
+    message.innerText = ``
+    pushButton.disabled = true
+    longPushButton.disabled = true
+
+    const token = await fetchAPI(`/push${isLong ? "?long":""}`, {
+        method: "POST"
+    })
+    if (token instanceof Error) {
+        message.innerText = token.message
+        message.classList.add("error")
+        pushButton.disabled = false
+        longPushButton.disabled = false
+        return
+    }
+
+    console.log(token)
+
+    const result = await fetchAPI(`/push/status`, {
+        method: "GET",
+        headers: {
+            "Push-Token": token
+        }
+    })
+    if (result instanceof Error) {
+        message.innerText = result.message
+        message.classList.add("error")
+        pushButton.disabled = false
+        longPushButton.disabled = false
+        return
+    }
+    
+    message.innerText = `操作が完了しました`
+    message.classList.remove("error")
+    pushButton.disabled = false
+    longPushButton.disabled = false
+
+}
+
+
+pushButton?.addEventListener("click", async () => {await buttonClickHandler(false)})
+longPushButton?.addEventListener("click", async () => {await buttonClickHandler(true)})
