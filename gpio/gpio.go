f4e8d52364d4f3d962afff351ae2ad9ac84869eb
OKAMOTO Shigehiro <primary@oka4shi.dev>
2024-11-24 15:26:36 +0900

    add dry-run function to gpio controller

--- gpio/gpio.go
+++ gpio/gpio.go
@@ -3,6 +3,7 @@ package gpio
 import (
 	"errors"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"sync"
@@ -14,9 +15,31 @@ type Port struct {
 	Locked bool
 }
 
-func isPortReady(pin int) bool {
-	gpioFile, err := os.Stat(fmt.Sprintf("/sys/class/gpio/gpio%d", pin))
+var DryRun = false
+
+func writeSysFile(path string, data string) error {
+	if DryRun {
+		log.Printf("Write \"%s\" to %s", data, path)
+		return nil
+	}
+
+	err := os.WriteFile(path, []byte(data), 0644)
+	return err
+}
+
+func doesFileExist(path string) bool {
+	if DryRun {
+		log.Printf("Check existance of %s", path)
+		return true
+	}
+
+	gpioFile, err := os.Stat(path)
 	return err == nil && gpioFile.IsDir()
+
+}
+
+func isPortReady(pin int) bool {
+	return doesFileExist(fmt.Sprintf("/sys/class/gpio/gpio%d", pin))
 }
 
 func NewPort(bank Bank, group Group, x X) (*Port, error) {
@@ -24,8 +47,11 @@ func NewPort(bank Bank, group Group, x X) (*Port, error) {
 	p := Port{
 		Pin: pin,
 	}
-	export, err := os.Stat("/sys/class/gpio/export")
-	if err != nil || export.IsDir() {
+	if DryRun {
+		return &p, nil
+	}
+
+	if !doesFileExist("/sys/class/gpio/export") {
 		return nil, errors.New("Can't use GPIO")
 	}
 
@@ -33,7 +59,7 @@ func NewPort(bank Bank, group Group, x X) (*Port, error) {
 		return &p, nil
 	}
 
-	if err := os.WriteFile("/sys/class/gpio/export", []byte(strconv.Itoa(pin)), 0644); err != nil {
+	if err := writeSysFile("/sys/class/gpio/export", strconv.Itoa(pin)); err != nil {
 		return nil, errors.New("Failed to initialize GPIO")
 	}
 	return &p, nil
@@ -65,7 +91,7 @@ func (p *Port) SetDirection(value Direction) error {
 	if !isPortReady(p.Pin) {
 		return errors.New("The port isn't initialized")
 	}
-	if err := os.WriteFile(fmt.Sprintf("/sys/class/gpio/gpio%d/direction", p.Pin), []byte(directionMap[value]), 0644); err != nil {
+	if err := writeSysFile(fmt.Sprintf("/sys/class/gpio/gpio%d/direction", p.Pin), directionMap[value]); err != nil {
 		return errors.New("Failed to set the direction")
 	}
 
@@ -76,7 +102,7 @@ func (p *Port) DegitalWrite(value OutLevel) error {
 	if !isPortReady(p.Pin) {
 		return errors.New("The port isn't initialized")
 	}
-	if err := os.WriteFile(fmt.Sprintf("/sys/class/gpio/gpio%d/value", p.Pin), []byte(fmt.Sprintf("%d", value)), 0644); err != nil {
+	if err := writeSysFile(fmt.Sprintf("/sys/class/gpio/gpio%d/value", p.Pin), fmt.Sprintf("%d", value)); err != nil {
 		return errors.New("Failed to set value")
 	}
 
