944c3bac76331a73a5ea6859664ff458f74ec14b
OKAMOTO Shigehiro <primary@oka4shi.dev>
2025-09-21 19:56:10 +0900

    [client] feat: change to the SSE endpoint and add animatino

--- frontend/src/lib/fetch.ts
+++ frontend/src/lib/fetch.ts
@@ -24,3 +24,101 @@ export async function fetchAPI(path: string, init: RequestInit) {
 
   return res;
 }
+
+export function startStream(
+  path: string,
+  timeout: number,
+  retryIn: number,
+  onStart: (path: string, retryTimes: number) => void,
+  onOpen: (event: Event) => void,
+  onMessage: (data: string) => void,
+  onHeartbeat: () => void,
+  onTimeout: (message: string) => void,
+  retryTimes: number = 0
+): EventSource | null {
+  onStart(path, retryTimes);
+  let errorTimeout: number | undefined = undefined;
+  const eventSource = new EventSource(path);
+
+  errorTimeout = setTimeout(
+    () => {
+      onTimeout(
+        `Can't connect to stream in ${retryIn * (retryTimes * 0.5 + 1)} milliseconds`
+      );
+      stopStream(eventSource, errorTimeout);
+      startStream(
+        path,
+        timeout,
+        retryIn,
+        onStart,
+        onOpen,
+        onMessage,
+        onHeartbeat,
+        onTimeout,
+        retryTimes + 1
+      );
+    },
+    retryIn * (retryTimes * 0.5 + 1)
+  );
+
+  eventSource.onopen = (event) => {
+    clearTimeout(errorTimeout);
+    retryTimes = 0;
+    onOpen(event);
+  };
+
+  eventSource.onmessage = (event) => {
+    clearTimeout(errorTimeout);
+    onMessage(event.data);
+  };
+
+  eventSource.addEventListener("heartbeat", () => {
+    clearTimeout(errorTimeout);
+    onHeartbeat();
+
+    errorTimeout = setTimeout(() => {
+      onTimeout(`No heartbeat received in ${timeout} milliseconds`);
+      stopStream(eventSource, errorTimeout);
+      startStream(
+        path,
+        timeout,
+        retryIn,
+        onStart,
+        onOpen,
+        onMessage,
+        onHeartbeat,
+        onTimeout
+      );
+    }, timeout);
+  });
+
+  eventSource.onerror = () => {
+    onTimeout("Stream error occurred");
+    clearTimeout(errorTimeout);
+
+    // Retry connection instantly
+    stopStream(eventSource, errorTimeout);
+    startStream(
+      path,
+      timeout,
+      retryIn,
+      onStart,
+      onOpen,
+      onMessage,
+      onHeartbeat,
+      onTimeout
+    );
+  };
+
+  return eventSource;
+}
+
+function stopStream(
+  eventSource: EventSource,
+  errorTimeout: number | undefined
+) {
+  if (eventSource) {
+    eventSource.close();
+  }
+  clearTimeout(errorTimeout);
+}
